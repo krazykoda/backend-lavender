b444763932f4c12c27988d8a7c960b745b653a8e
kryzbone <kryzbone@gmail.com>
2025-04-03 23:33:34 +0000

    added views for tasks

--- django_api/task_app/urls.py
+++ django_api/task_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import create_task
+from .views import create_task, list_tasks, get_task, update_task
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('add/', csrf_exempt(create_task)),
+    path("list/", list_tasks, name="list_tasks"),
+    path("get/<int:task_id>/", get_task),
+    path("update/<int:task_id>/", csrf_exempt(update_task))
 ]
--- django_api/task_app/views.py
+++ django_api/task_app/views.py
@@ -1,18 +1,92 @@
 import json
 
+from django.core import serializers
 from django.http import HttpResponse, JsonResponse
 
 from .models import Tasks
 
 # Create your views here.
 def create_task(request):
-    raw_data = request.body
-    conv_data = json.loads(request.body)
-    # save to db
-    data = Tasks(**conv_data)
-    data.save()
+    if request.method == "POST":
+        # request.body is a json
+        raw_data = request.body
+       
 
-    return JsonResponse({"data": "data created succesfully"}, status=201)
+        # convert json into dictionary
+        conv_data = json.loads(raw_data)
+
+        print(conv_data)
+        
+
+        name = conv_data.get("name")
+        description = conv_data.get("description")
+
+        # check if request body has name
+        if not name:
+            return JsonResponse({"error": "Please provide the name"}, status=400)
+
+
+        # save to db
+        # data = Tasks(name=name, description=description)
+        # data.save()
+        data = Tasks.objects.create(**conv_data)   
+
+        json_data = serializers.serialize('json', data) 
+
+        return JsonResponse({"data": "data created succesfully"}, status=201)
+
+    else:
+        return JsonResponse({"error": "Method not allowed"}, status=405)
+
+
+
+# List Tasks
+def list_tasks(request):
+    if request.method == "GET":
+        data = Tasks.objects.all()
+
+        # serialize query response
+        json_data = serializers.serialize('json', data) 
+        json_data = json.loads(json_data)
+
+        return JsonResponse({"data": json_data}, status=200)
+
+    else:
+        return JsonResponse({"error": "Method not allowed"}, status=405)
+
+
+
+# Get One Task
+def get_task(request, task_id):
+    print(task_id)
+    data = Tasks.objects.filter(pk=task_id).first()
+    
+    # You can only serialize a list
+    json_data = serializers.serialize('json', [data]) 
+    json_data = json.loads(json_data)
+
+    return JsonResponse({"data": json_data}, status=200)
+
+
+
+# Update a task
+def update_task(request, task_id):
+    raw_data = request.body
+    dict_data = json.loads(raw_data)
+
+    # csome validations 
+
+    # Update task
+    Tasks.objects.filter(pk=task_id).update(**dict_data)
+
+    return JsonResponse({"message": "task updated"}, status=200)
+
+
+
+
+
+# Delete a task
+
 
 
    
